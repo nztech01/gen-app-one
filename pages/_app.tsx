4064aff7bad582840efcc980953ab2811ec69d65
nztech01 <ian@computerdoctor.online>
2023-03-18 09:29:21 +1300

    new Changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import 'npm/styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+
+  This is the home page
+  </>
+ 
+ 
+
+
 }
