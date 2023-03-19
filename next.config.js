d3d77c7e213d8fdc8634a5d39724957d7ce27f3e
nztech01 <ian@computerdoctor.online>
2023-03-20 12:18:04 +1300

    update

--- next.config.js
+++ next.config.js
@@ -8,3 +8,11 @@ module.exports = {
     apiKey: process.env.API_KEY}
 
 };
+
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
